af5f5c1205cd97bec05c9f60735e070ea7d9e523
davidpatsina <dpats19@freeuni.edu.ge>
2024-01-23 01:56:50 +0400

    Delete old invoice class and and change local data

--- InvoiceAPP_API/InvoiceAPP/Data/InvoiceStore.cs
+++ InvoiceAPP_API/InvoiceAPP/Data/InvoiceStore.cs
@@ -1,13 +1,30 @@
 ﻿using InvoiceAPP.Models.DTO;
+using InvoiceAPP.Models.Invoice;
 
 namespace InvoiceAPP.Data
 {
     public static class InvoiceStore
     {
-        public static  List<InvoiceDTO> invoiceList = new List<InvoiceDTO>
+        public static  List<Invoice> invoiceList = new List<Invoice>
         {
-                new InvoiceDTO{InvoiceId = 1, Amount = 1000.0, FromUserID = 1, ToUserID = 3},
-                new InvoiceDTO{InvoiceId = 2, Amount = 1500.0, FromUserID = 2,ToUserID  = 3}
+          //  string invoiceId,  string ownerId, Address billFrom, CustomerInfo billTo, ProjectInfo projectInfo, List<Item> itemList, Status status
+                new Invoice(
+                            "XG1234", 
+                    "1", 
+                    new Address("Rustaveli", "Tbilisi", "QWE12 PD3", "Tbilisi"),
+                      new CustomerInfo("Nick", "cafe@gmail.com", new Address("Mardjanishvili", "Tbilsi", "QW21R", "Tbilisi")),
+                            new ProjectInfo(DateTime.Today, 45, "Baris Magidebu"),
+                            new List<Item>{new Item("Magida #1", 3, 40.0), new Item("Magida #2", 4, 30.0)},
+                            Invoice.Status.PAID),
+                new Invoice("ZW4349", 
+                    "1", 
+                    new Address("Tsereteli", "Tbilisi", "STE56 PR4", "Tbilisi"),
+                    new CustomerInfo("David", "building@gmail.com", new Address("Chavchavadze", "Tbilsi", "QW21R", "Tbilisi")),
+                    new ProjectInfo(DateTime.Today, 45, "Ciklovka"),
+                    new List<Item>{new Item("Iatakis laqi", 5, 10.0), new Item("Rastvaliweli", 2, 20.0)},
+                    Invoice.Status.DRAFT),
+                
+                
         };
     }
 }
