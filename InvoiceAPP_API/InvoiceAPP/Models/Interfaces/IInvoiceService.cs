af3e1776f75edc1391c6bb2cf8963113018ecfcd
davidpatsina <dpats19@freeuni.edu.ge>
2024-01-23 16:06:10 +0400

    Edit Inoviceinterface

--- InvoiceAPP_API/InvoiceAPP/Models/Interfaces/IInvoiceService.cs
+++ InvoiceAPP_API/InvoiceAPP/Models/Interfaces/IInvoiceService.cs
@@ -5,11 +5,18 @@ namespace InvoiceAPP.Models
 {
     public interface IInvoiceService
     {
-        public ActionResult<Invoice.Invoice> newInvoice(Invoice.Invoice invoiceToCreate);
-        public IActionResult editInvoice(string invoiceId, Invoice.Invoice editedInvoice);
-        public IActionResult markAsPaid(string invoiceId, Invoice.Invoice.Status newStatus);
-        public IActionResult deleteInvoice(string invoiceId);
-        public ActionResult<List<Invoice.Invoice> > getInvoicesByOwnerId(string ownerId);
-        public  ActionResult<List<Invoice.Invoice> > GetInvoicesByStatus(string ownwerId, Invoice.Invoice.Status status);
+        public enum STATUS_CODE
+        {
+            BAD_REQUEST,
+            NOT_FOUND,
+            OK,
+            NO_CONTENT
+        }
+        public KeyValuePair<STATUS_CODE, Invoice.Invoice> newInvoice(Invoice.Invoice invoiceToCreate);
+        public KeyValuePair<STATUS_CODE, Invoice.Invoice> editInvoice(string invoiceId, Invoice.Invoice editedInvoice);
+        public KeyValuePair<STATUS_CODE, Invoice.Invoice> markAsPaid(string invoiceId, Invoice.Invoice.Status newStatus);
+        public KeyValuePair<STATUS_CODE, Invoice.Invoice> deleteInvoice(string invoiceId);
+        public KeyValuePair<STATUS_CODE, List<Invoice.Invoice> >  getInvoicesByOwnerId(string ownerId);
+        public  KeyValuePair<STATUS_CODE, List<Invoice.Invoice> > GetInvoicesByStatus(string ownwerId, Invoice.Invoice.Status status);
     }
 }
