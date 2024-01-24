e002a08250d31fb6a9792bad892972e531ec0e6b
davidpatsina <dpats19@freeuni.edu.ge>
2024-01-24 14:35:55 +0400

    add custom route constraint for enum

--- InvoiceAPP_API/InvoiceAPP/Program.cs
+++ InvoiceAPP_API/InvoiceAPP/Program.cs
@@ -1,34 +1,55 @@
 using InvoiceAPP.Models;
 using InvoiceAPP.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+});
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();;
+
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 builder.Services.AddScoped<IInvoiceService, InvoiceService>();
 
-
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.ConstraintMap.Add("Invoice.Status", typeof(InvoiceAPP.Controllers.EnumRoutes.Extensions.CustomRouteConstraint<InvoiceAPP.Models.Invoice.Invoice.Status>));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+    });
 }
-
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseExceptionHandler("/Home/Error");
 
 app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
