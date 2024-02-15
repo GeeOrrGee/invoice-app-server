f4c51b01ddbb782ba129a2748d0c5427181580b0
KharebaKavtaradze <kkavt21@freeuni.edu.ge>
2024-02-15 15:25:29 +0400

    Unsaved dbcontext

--- InvoiceAPP/Data/InvoiceDbContext.cs
+++ InvoiceAPP/Data/InvoiceDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace InvoiceAPP.Data
+﻿using InvoiceAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceAPP.Data
 {
-    public class MyDbContext
+    public class InvoiceDbContext : DbContext
     {
+        public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<InvoiceEntity> Invoices { get; set; }
+        public DbSet<AdressEntity> Adresses { get; set; }
+        public DbSet<ItemEntity> Items { get; set; }
     }
 }
