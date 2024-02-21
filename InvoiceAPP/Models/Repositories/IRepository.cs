014176634b0a3c20f29ae68aacd0bc671aa5f879
KharebaKavtaradze <kkavt21@freeuni.edu.ge>
2024-02-21 15:09:00 +0400

    Repositories for invoices, items and addresses

--- InvoiceAPP/Models/Repositories/IRepository.cs
+++ InvoiceAPP/Models/Repositories/IRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace InvoiceAPP.Models.Repositories
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        T GetById(string id);
+        IEnumerable<T> GetList();
+        IEnumerable<T> GetIList(string id);
+        bool Create(T item);
+        void Update(T item);
+        void Delete(string id);
     }
 }
