7133b44264da262c3f3c0a3af6f05133e801df54
KharebaKavtaradze <kkavt21@freeuni.edu.ge>
2024-02-21 15:41:09 +0400

    DTO objects for invoices, items and addresses

---
+++ InvoiceAPP/Models/DTO/AddressDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceAPP.Models.DTO
+{
+    public class AddressDTO
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string StreetAddress { get; set; }
+        public string PostCode { get; set; }
+    }
+}
